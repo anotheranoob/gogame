5c01b49097ed43d4c9d4fc2a4ce27580b02c9ce2
anotheranoob <k447038@gmail.com>
2017-09-14 12:50:42 -0400

    Create go.py

---
+++ go.py
@@ -0,0 +1,13 @@
+'''
+Author : Kevin Wu
+This program plays the game go
+It has a goSquare class, a goGrid class, and a goFrame class.
+The goSquare is an individual square from the goGame which has an attribute
+for its position and color. It has methods for drawing a circle and removing one.
+
+The goGrid contains all of the goSquares and also handles making moves and the UI
+
+The goFrame handles all of the logic and end of game stuff.
+
+'''
+from tkinter import *
