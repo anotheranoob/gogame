1987ad51ea5bf22f815e7eea727ed5aa849ee61f
anotheranoob <k447038@gmail.com>
2017-09-27 12:12:29 -0400

    Update go.py

--- go.py
+++ go.py
@@ -14,6 +14,30 @@ from tkinter import *
 def _create_circle(self, x, y, r, **kwargs):
     return self.create_oval(x-r, y-r, x+r, y+r, **kwargs)
 Canvas.create_circle = _create_circle
+def set_aspect(content_frame, pad_frame, aspect_ratio):
+    # a function which places a frame within a containing frame, and
+    # then forces the inner frame to keep a specific aspect ratio
+
+    def enforce_aspect_ratio(event):
+        # when the pad window resizes, fit the content into it,
+        # either by fixing the width or the height and then
+        # adjusting the height or width based on the aspect ratio.
+
+        # start by using the width as the controlling dimension
+        desired_width = event.width
+        desired_height = int(event.width / aspect_ratio)
+
+        # if the window is too tall to fit, use the height as
+        # the controlling dimension
+        if desired_height > event.height:
+            desired_height = event.height
+            desired_width = int(event.height * aspect_ratio)
+
+        # place the window, giving it an explicit size
+        content_frame.place(in_=pad_frame, x=0, y=0, 
+            width=desired_width, height=desired_height)
+
+    pad_frame.bind("<Configure>", enforce_aspect_ratio)
 class goSquare(Canvas):
     def __init__(self, position, master):
         self.master=master
@@ -24,11 +48,20 @@ class goSquare(Canvas):
         self.create_line(15,0,15,30)
         self.color=None
         self.bind("<Button-1>", master.get_click)
+        self.bind("<Configure>", self.on_resize)
     def make_piece(self, color):
+        if color==None:
+            return
         self.color=color
-        self.piece=self.create_circle(15,15,13, fill=color)
+        self.piece=self.create_circle(self.winfo_width()/2,self.winfo_width()/2,self.winfo_width()*13/30, fill=color)
     def clear(self):
         self.delete(self.piece)
+        self.color=None
+    def on_resize(self,event):
+        self.delete("all")
+        self.create_line(0,self.winfo_width()/2, self.winfo_height(),self.winfo_width()/2)
+        self.create_line(self.winfo_height()/2, 0,self.winfo_height()/2,self.winfo_width())
+        self.make_piece(self.color)
 
 class goGrid(Frame):
     def __init__(self, master):
@@ -45,8 +78,16 @@ class goGrid(Frame):
     def get_click(self, event):
         event.widget.clear()
 
-bob=Tk()
-goGrid(bob).grid(sticky=N+S+E+W, row=0, column=0)
-Grid.rowconfigure(bob, 0, weight=1)
-Grid.columnconfigure(bob, 0, weight=1)
-bob.mainloop()
+class goFrame(Frame):
+    def __init__(self):
+        self.master=Tk()
+        self.master.rowconfigure(0, weight=1)
+        self.master.columnconfigure(0, weight=1)
+        Frame.__init__(self, self.master)
+        self.grid(row=0, column=0, sticky="nsew")
+        self.goGrid=goGrid(self)
+        set_aspect(self.goGrid, self, 1.0)
+        self.master.geometry('300x300')
+        self.master.mainloop()
+
+goFrame()
